626bd6a509c5d42d95ed6f0acc701b81c74d0599
Papoel <pascal.briffard@gmail.com>
2024-04-14 10:40:39 +0200

    ♻️ REFACTOR: function hover effect in function file

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -90,7 +90,7 @@ let e = {
 			e.customScrollbar(),
 			e.pswMeter(),
 			e.menuDropdown()
-			//e.cardHoverEffect();
+			e.cardHoverEffect();
 	},
 	isVariableDefined: function (el) {
 		return typeof !!el && (el) != 'undefined' && el != null;
@@ -771,56 +771,24 @@ let e = {
 	},
 	// END: Menu Dropdown
 
-	/*
-	    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+	//START: 20 Card Hover Effect
+	cardHoverEffect: function () {
+		function getRandomColor() {
+			let letters = '0123456789ABCDEF';
+			let color = '#';
+			for (let i = 0; i < 6; i++) {
+				color += letters[Math.floor(Math.random() * 16)];
+			}
+			return color;
+		}
 
-    document.querySelectorAll('.custom-card').forEach(function(card) {
-        card.addEventListener('mouseenter', function() {
-            var randomColor = getRandomColor();
-            this.style.boxShadow = '0 0 50px ' + randomColor;
-        });
-
-        card.addEventListener('mouseleave', function() {
-            this.style.boxShadow = '';
-        });
-    });
-	 */
-
-	// START: 20 Card Hover Effect
-	// cardHoverEffect: function () {
-	// 	function getRandomColor() {
-	// 		let letters = '0123456789ABCDEF';
-	// 		let color = '#';
-	// 		for (let i = 0; i < 6; i++) {
-	// 			color += letters[Math.floor(Math.random() * 16)];
-	// 		}
-	// 		return color;
-	// 	}
-	//
-	// 	document.querySelectorAll('.custom-card').forEach(function(card) {
-	// 		card.addEventListener('mouseenter', function() {
-	// 			let randomColor = getRandomColor();
-	// 			this.style.boxShadow = '0 0 30px ' + randomColor;
-	// 		});
-	//
-	// 		card.addEventListener('mouseleave', function() {
-	// 			this.style.boxShadow = '';
-	// 		});
-	//
-	// 		card.querySelectorAll('button').forEach(function(button) {
-	// 			button.addEventListener('mouseenter', function(event) {
-	// 				event.stopPropagation();
-	// 			});
-	// 		});
-	// 	});
-	// },
+		document.querySelectorAll('.custom-card').forEach(function(card) {
+			card.addEventListener('mouseenter', function() {
+				let randomColor = getRandomColor();
+				this.style.setProperty('--random-color', randomColor);
+			});
+		});
+	},
 };
 
 e.init();
