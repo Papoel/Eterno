559feab0c4e958ea8ed4c7dfe1d53745bd62038e
Papoel <pascal.briffard@gmail.com>
2025-05-03 10:02:16 +0200

    📦 DEP: Ajout de Monolog

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
